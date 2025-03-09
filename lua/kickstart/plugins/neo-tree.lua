60feff5158d38ec924c2de0369b1e54376b75641
Faiyt <unfaiyted@gmail.com>
2025-03-09 09:49:17 -0500

    chore: updated settings

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -4,6 +4,12 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   cmd = 'Neotree',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
   keys = {
     {
       '<leader>fe',
